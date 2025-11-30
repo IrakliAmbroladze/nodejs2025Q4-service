5ea6a05ebf0d8a7a4535eae344efb54b4fa072a0
Irakli Ambroladze <ambroladzeirakli@gmail.com>
2025-11-30 19:41:05 +0400

    feat: add cascade delete logic in db service

--- src/database/database.service.ts
+++ src/database/database.service.ts
@@ -15,4 +15,43 @@ export class DatabaseService {
     albums: [] as string[],
     tracks: [] as string[],
   };
+
+  deleteArtist(artistId: string): void {
+    const artistFavIndex = this.favorites.artists.indexOf(artistId);
+    if (artistFavIndex !== -1) {
+      this.favorites.artists.splice(artistFavIndex, 1);
+    }
+
+    this.albums.forEach((album) => {
+      if (album.artistId === artistId) {
+        album.artistId = null;
+      }
+    });
+
+    this.tracks.forEach((track) => {
+      if (track.artistId === artistId) {
+        track.artistId = null;
+      }
+    });
+  }
+
+  deleteAlbum(albumId: string): void {
+    const albumFavIndex = this.favorites.albums.indexOf(albumId);
+    if (albumFavIndex !== -1) {
+      this.favorites.albums.splice(albumFavIndex, 1);
+    }
+
+    this.tracks.forEach((track) => {
+      if (track.albumId === albumId) {
+        track.albumId = null;
+      }
+    });
+  }
+
+  deleteTrack(trackId: string): void {
+    const trackFavIndex = this.favorites.tracks.indexOf(trackId);
+    if (trackFavIndex !== -1) {
+      this.favorites.tracks.splice(trackFavIndex, 1);
+    }
+  }
 }
