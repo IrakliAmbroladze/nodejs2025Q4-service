ec245aa61a207b4538bd9f55c438b8fcc95af47b
Irakli Ambroladze <ambroladzeirakli@gmail.com>
2025-11-30 18:36:26 +0400

    feat: create artist service

---
+++ src/artists/artists.service.ts
@@ -0,0 +1,61 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { randomUUID } from 'crypto';
+import { Artist } from './entities/artist.entity';
+import { CreateArtistDto, UpdateArtistDto } from './dto/artist.dto';
+import { DatabaseService } from '../database/database.service';
+
+@Injectable()
+export class ArtistsService {
+  constructor(private readonly db: DatabaseService) {}
+
+  create(createArtistDto: CreateArtistDto): Artist {
+    const newArtist: Artist = {
+      id: randomUUID(),
+      name: createArtistDto.name,
+      grammy: createArtistDto.grammy,
+    };
+
+    this.db.artists.push(newArtist);
+    return newArtist;
+  }
+
+  findAll(): Artist[] {
+    return this.db.artists;
+  }
+
+  findOne(id: string): Artist {
+    const artist = this.db.artists.find((artist) => artist.id === id);
+    if (!artist) {
+      throw new NotFoundException('Artist not found');
+    }
+    return artist;
+  }
+
+  update(id: string, updateArtistDto: UpdateArtistDto): Artist {
+    const artistIndex = this.db.artists.findIndex((artist) => artist.id === id);
+    if (artistIndex === -1) {
+      throw new NotFoundException('Artist not found');
+    }
+
+    const updatedArtist: Artist = {
+      id,
+      name: updateArtistDto.name,
+      grammy: updateArtistDto.grammy,
+    };
+
+    this.db.artists[artistIndex] = updatedArtist;
+    return updatedArtist;
+  }
+
+  remove(id: string): void {
+    const artistIndex = this.db.artists.findIndex((artist) => artist.id === id);
+    if (artistIndex === -1) {
+      throw new NotFoundException('Artist not found');
+    }
+    this.db.artists.splice(artistIndex, 1);
+  }
+
+  exists(id: string): boolean {
+    return this.db.artists.some((artist) => artist.id === id);
+  }
+}
