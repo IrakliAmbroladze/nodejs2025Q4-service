7e3e60bd0e39d22e1304064aa4195a3bd7f0b04d
Irakli Ambroladze <ambroladzeirakli@gmail.com>
2025-11-30 23:04:33 +0400

    refac: make artists controller asynchronous

--- src/artists/artists.controller.ts
+++ src/artists/artists.controller.ts
@@ -20,37 +20,40 @@ export class ArtistsController {
 
   @Post()
   @HttpCode(HttpStatus.CREATED)
-  create(@Body() createArtistDto: CreateArtistDto) {
-    return this.artistsService.create(createArtistDto);
+  async create(@Body() createArtistDto: CreateArtistDto) {
+    return await this.artistsService.create(createArtistDto);
   }
 
   @Get()
-  findAll() {
-    return this.artistsService.findAll();
+  async findAll() {
+    return await this.artistsService.findAll();
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
+  async findOne(@Param('id') id: string) {
     if (!isUUID(id)) {
       throw new BadRequestException('Invalid artist ID (not UUID)');
     }
-    return this.artistsService.findOne(id);
+    return await this.artistsService.findOne(id);
   }
 
   @Put(':id')
-  update(@Param('id') id: string, @Body() updateArtistDto: UpdateArtistDto) {
+  async update(
+    @Param('id') id: string,
+    @Body() updateArtistDto: UpdateArtistDto,
+  ) {
     if (!isUUID(id)) {
       throw new BadRequestException('Invalid artist ID (not UUID)');
     }
-    return this.artistsService.update(id, updateArtistDto);
+    return await this.artistsService.update(id, updateArtistDto);
   }
 
   @Delete(':id')
   @HttpCode(HttpStatus.NO_CONTENT)
-  remove(@Param('id') id: string) {
+  async remove(@Param('id') id: string) {
     if (!isUUID(id)) {
       throw new BadRequestException('Invalid artist ID (not UUID)');
     }
-    return this.artistsService.remove(id);
+    await this.artistsService.remove(id);
   }
 }
