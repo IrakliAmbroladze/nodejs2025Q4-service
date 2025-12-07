8f9b2d50188ebe152869ae77587952c3b0c21cc5
Irakli Ambroladze <ambroladzeirakli@gmail.com>
2025-12-07 19:14:49 +0400

    refac: update types to Classes for typeorm

--- src/albums/entities/album.entity.ts
+++ src/albums/entities/album.entity.ts
@@ -1,6 +1,32 @@
-export type Album = {
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  ManyToOne,
+  OneToMany,
+  JoinColumn,
+} from 'typeorm';
+import { Artist } from '../../artists/entities/artist.entity';
+import { Track } from '../../tracks/entities/track.entity';
+
+@Entity('albums')
+export class Album {
+  @PrimaryGeneratedColumn('uuid')
   id: string;
+
+  @Column()
   name: string;
+
+  @Column()
   year: number;
+
+  @Column({ type: 'uuid', nullable: true })
   artistId: string | null;
-};
+
+  @ManyToOne(() => Artist, (artist) => artist.albums, { onDelete: 'SET NULL' })
+  @JoinColumn({ name: 'artistId' })
+  artist: Artist;
+
+  @OneToMany(() => Track, (track) => track.album)
+  tracks: Track[];
+}
