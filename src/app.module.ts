57067bada4f69f90f2085d3e21df5709e57927aa
Irakli Ambroladze <ambroladzeirakli@gmail.com>
2025-12-07 20:14:41 +0400

    feat: add typeorm module in app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { DatabaseModule } from './database/database.module';
+import { typeOrmConfig } from './config/typeorm.config';
 import { UsersModule } from './users/users.module';
 import { ArtistsModule } from './artists/artists.module';
 import { AlbumsModule } from './albums/albums.module';
@@ -10,7 +12,10 @@ import { FavoritesModule } from './favorites/favorites.module';
 
 @Module({
   imports: [
-    DatabaseModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRoot(typeOrmConfig),
     UsersModule,
     ArtistsModule,
     AlbumsModule,
